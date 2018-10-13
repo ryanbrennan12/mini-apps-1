cc13b95f65def79185c78d71b66d8c21e535fb67
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-10-13 13:14:16 -0700

    [WIP] - added event listeners to all squares

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,17 @@
-// let newDiv = document.createElement('div')
-let body = document.body
+///create a div
+///create a row
+///render the row
+document.querySelectorAll('.cell')
+.forEach(e => e.addEventListener('click', () => {
+  console.log('clicked')
+}))
 
+
+
+
+
+// document.querySelectorAll('.cell')
+// .forEach(e => e.addEventListener("click", function() {
+//     // Here, `this` refers to the element the event was hooked on
+//     console.log("clicked")
+// }));
