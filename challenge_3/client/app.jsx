a87556e36b0e0c165c07b44ce4792a39ef6fc446
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-10-15 12:22:09 -0700

    [WIP]- All dependencies installed and wired -index.html rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,29 @@
+class App extends React.Component {
+  constructor() {
+    super() 
+    this.state = {
+      step: 1
+    }
+    this.nextStep = this.nextStep.bind(this)
+  }
 
+  nextStep() {
+    
+  }
+
+  render() {
+    return (
+      <div>Heyyyy</div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express()
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+
+
+app.listen(port, () => {
+  console.log(`Hey, we are listening to ${port}`)
+})
+
 
 
