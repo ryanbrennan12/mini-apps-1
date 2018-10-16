6e937368871a209ee5930d52465e1544374fbe02
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-10-15 17:04:26 -0700

    [WIP] refactoring to have state in one component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,69 +2,130 @@ class App extends React.Component {
   constructor() {
     super() 
     this.state = {
-      step: 1
+      step: 1,
+      //form1
+      name: '',
+      email: '',
+      password: '',
+      //form 2
+      addy1: '',
+      addy2: '',
+      city: '',
+      state: '',
+      zip: '',
+      phone: '',
+      //form 3
+      credit: '',
+      exp: '',
+      cvv: '',
+      zip2: ''
     }
     this.nextStep = this.nextStep.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
-
-  
   //this will be triggered by Button in component
+  // saving to the DB here as well
   nextStep() {
-    console.log('I have been clicked!')
-    
+    console.log('clicked')
+    const {step} = this.state.step;
+    if (step !==3) {
+      this.setState({
+        step: step + 1
+      })
+    } else {
+      //SAVE TO DB
+      const values = {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        addy1: this.state.addy1,
+        addy2: this.state.addy2,
+        city: this.state.city,
+        state: this.state.state,
+        zip: this.state.zip,
+        phone: this.state.phone,
+        credit: this.state.credit,
+        exp: this.state.exp,
+        cvv: this.state.cvv,
+        zip2: this.state.zip2
+      }
+
+    }
+  }
+  //dont have to bind
+  handleChange(key) {
+    return (e) => this.setState({ [key]: e.target.value })
   }
 
   render() {
     return (
-      <Form1 onNext={this.nextStep}/>
+      <Form1 handleChange={this.handleChange} nextStep={this.nextStep} />
     )
   }
 }
-
+///FORM 1
 class Form1 extends React.Component {
   constructor() {
     super()
-    this.state = {
-      name: '',
-      email: '',
-      pass: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  ///rather than multiple event hanlders
-  handleChange(key) {
-    return function (e) {
-      console.log(e.target.value)
-      var state = {};
-      state[key] = e.target.value;
-      this.setState(state);
-    }.bind(this)
-  }
-  
-  handleSubmit(e) {
-    let data = {
-      name: this.state.name,
-      email: this.state.email,
-      pass: this.state.pass,
-    }
-  }  
-  
+
   render() {
     return (
-    <div>
-          Name: 
-          <input value={this.state.name} onChange={this.handleChange('name')} />
-          Email: 
-          <input value={this.state.email} onChange={this.handleChange('email')} />
-          Password:
-          <input value={this.state.message} onChange={this.handleChange('message')} />
-          <button onClick={() => this.props.onNext()}>Next</button>
-       </div>
-    );
+      <div>
+      Name:  
+      <input onChange={this.props.handleChange('name')}></input>
+      Email: 
+      <input onChange={this.props.handleChange('email')}></input>
+      Password: 
+      <input onChange={this.props.handleChange('password')}></input>
+      <button onClick={()=> this.props.nextStep()}>Next</button>
+      </div>
+    )
   }
 }
-        
+class Form2 extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+      Address:  
+      <input onChange={this.props.handleChange('addy1')}></input>
+      <input onChange={this.props.handleChange('addy2')}></input>
+      City: 
+      <input onChange={this.props.handleChange('city')}></input>
+      State: 
+      <input onChange={this.props.handleChange('state')}></input>
+      Zip: 
+      <input onChange={this.props.handleChange('zip')}></input>
+      Phone: 
+      <input onChange={this.props.handleChange('phone')}></input>
+      </div>
+    )
+  }
+}
+// class Form1 extends React.Component {
+//   constructor() {
+//     super()
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//       Credit Card Number:  
+//       <input onChange={this.props.handleChange('credit')}></input>
+//       Expiration Date: 
+//       <input onChange={this.props.handleChange('exp')}></input>
+//       CVV Number: 
+//       <input onChange={this.props.handleChange('cvv')}></input>
+//       Billing Zip: 
+//       <input onChange={this.props.handleChange('zip2')}></input>
+//       </div>
+//     )
+//   }
+// }
 
 
 
