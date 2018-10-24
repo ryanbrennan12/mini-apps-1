6ee5a2483cbb2a1d14cf20595e15ff2d55249c80
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-10-23 18:58:47 -0700

    [WIP] packages installed, server running

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
 
 
+app.get('/', (req, res) => {
+  res.send('Hey hey here are some kind words!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Woop Woop we are listening on port ${port}!`)
+})
