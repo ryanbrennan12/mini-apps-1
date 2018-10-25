192a4d5d9fbb6463a8d91146f510b6d73fde33d8
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-10-25 14:13:01 -0700

    [WIP] - req.body coming through. oi

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.use(express.static('client'))
 
 const port = 3000;
 
 app.post("/transform", (req, res) => {
-  console.log('BODY BEB, ', req.body)
+  console.log(req.body)
 })
 
 
