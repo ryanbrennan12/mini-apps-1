e3945d81fc3d75b57c461b24832c339470d33153
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-10-25 16:14:05 -0700

    [WIP] parsing data

--- challenge_2/helper.js
+++ challenge_2/helper.js
@@ -1,8 +1,12 @@
 
 
 const converter = (data, cb) => {
-  let newData = JSON.stringify(data)
-  cb(newData)
+
+  let parsed = JSON.parse(data.data)
+  cb(parsed)
+  // let newData = JSON.stringify(data)
+
+
 }
 
 module.exports.converter = converter;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -8,6 +8,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+
+
+
 app.use(express.static('client'))
 
 const port = 3000;
